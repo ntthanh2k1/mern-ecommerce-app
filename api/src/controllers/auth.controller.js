f7e6541a338c54dddd576303b4ebb4b2bc598cb7
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-04-14 11:32:18 +0700

    Update code 14/4/2025.

---
+++ api/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const register = async (req, res) => {
+  console.log("register");
+};
+
+const login = async (req, res) => {
+
+};
+
+const logout = async (req, res) => {
+
+};
+
+export { register, login, logout };
---
+++ api/src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
